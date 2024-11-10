fb25b41de0a188b05ad7696b1a27b49da3845a1e
david-borja <davidborjamartinez@gmail.com>
2024-11-10 17:03:23 +0100

    feat(): hello world

--- final_project/settings.py
+++ final_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'splitwise_clone',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
