961cb221e3549642c998706fdb942fccdd2062cd
david-borja <davidborjamartinez@gmail.com>
2024-12-01 15:30:18 +0100

    feat(): expenses section

--- splitwise_clone/templatetags/custom_tags.py
+++ splitwise_clone/templatetags/custom_tags.py
@@ -1,5 +1,22 @@
 from django import template
+from django.conf import settings
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+import os
+
 register = template.Library()
+
+@register.simple_tag
+def svg_icon(file_name):
+    svg_path = os.path.join(
+        settings.BASE_DIR, "splitwise_clone/static/icons", file_name + ".svg"
+    )
+    with open(svg_path, "r") as file:
+        svg_code = file.read()
+
+    svg_code = format_html(svg_code)
+    return mark_safe(svg_code)
+
 
 @register.inclusion_tag("splitwise_clone/components/input-group.html")
 def custom_input(name, type, placeholder, label):
@@ -26,11 +43,32 @@ def balance_summary(icon, amount_str, summary_details):
     }
 
 
-@register.inclusion_tag("splitwise_clone/components/balance-feed-item.html")
-def balance_feed_item(char, username, balance, is_it_me):
+@register.inclusion_tag("splitwise_clone/components/balances-feed-item.html")
+def balances_feed_item(char, username, balance, is_it_me):
     return {
         "char": char,
         "username": username,
         "balance": balance,
         "is_it_me": is_it_me,
+    }
+
+@register.inclusion_tag("splitwise_clone/components/expenses-feed-item.html")
+def expenses_feed_item(date, icon, transaction_type, description, author, amount):
+    return {
+        "date": date,
+        "icon": icon,
+        "transaction_type": transaction_type,
+        "description": description,
+        "author": author,
+        "amount": amount,
+    }
+
+
+@register.inclusion_tag("splitwise_clone/components/footer-create-button.html")
+def footer_create_button(id, visibility, form_url, text):
+    return {
+        "id": id,
+        "visibility": visibility,
+        "form_url": form_url,
+        "text": text,
     }
