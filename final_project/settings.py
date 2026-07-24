e6ebc29322e78f817fb6ca1be790db0e1086e267
david-borja <davidborjamartinez@gmail.com>
2026-07-24 19:10:13 +0200

    fix(): update security settings for production

--- final_project/settings.py
+++ final_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-65b&=5rvm(tb$%wrt$==h5cxbzxggqpp1lpi0l&dh=ml0she+3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
